4eba315526b05fe72817843d92046299b5b46be8
Piotr Sikora <piotrsikora@google.com>
2020-10-26 10:51:20 +0000

    Allow building for wasm32-wasi target.
    Signed-off-by: Piotr Sikora <piotrsikora@google.com>

--- src/allocator.rs
+++ src/allocator.rs
@@ -15,8 +15,12 @@
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+#[cfg_attr(
+    all(target_arch = "wasm32", target_os = "unknown"),
+    export_name = "malloc"
+)]
 #[no_mangle]
-pub extern "C" fn malloc(size: usize) -> *mut u8 {
+pub extern "C" fn proxy_on_memory_allocate(size: usize) -> *mut u8 {
     let mut vec: Vec<u8> = Vec::with_capacity(size);
     unsafe {
         vec.set_len(size);
