abe786212c7ed242f193e930586cd07423e70e3f
Martijn Swaagman <martijn@swaagman.online>
2022-01-25 14:03:03 +0100

    chore: allow unsafe code to deal with uninitialized data
    Signed-off-by: Martijn Swaagman <martijn@swaagman.online>

--- src/allocator.rs
+++ src/allocator.rs
@@ -16,13 +16,15 @@
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+use std::mem::MaybeUninit;
+
 #[cfg_attr(
     all(target_arch = "wasm32", target_os = "unknown"),
     export_name = "malloc"
 )]
 #[no_mangle]
 pub extern "C" fn proxy_on_memory_allocate(size: usize) -> *mut u8 {
-    let mut vec: Vec<u8> = Vec::with_capacity(size);
+    let mut vec: Vec<MaybeUninit<u8>> = Vec::with_capacity(size);
     unsafe {
         vec.set_len(size);
     }
