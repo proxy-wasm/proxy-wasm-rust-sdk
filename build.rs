104bcb6c7064f74e046e83c2866c9c7f248a3212
Piotr Sikora <piotrsikora@google.com>
2022-04-05 01:25:01 -0700

    review: add cargo:rerun-if-changed to the build script.
    Signed-off-by: Piotr Sikora <piotrsikora@google.com>

--- build.rs
+++ build.rs
@@ -13,11 +13,15 @@
 // limitations under the License.
 
 fn main() {
+    println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rerun-if-env-changed=RUSTFLAGS");
+
     if let Some(target_os) = std::env::var_os("CARGO_CFG_TARGET_OS") {
         if target_os != "wasi" {
             return;
         }
     }
+
     if let Some(rustflags) = std::env::var_os("CARGO_ENCODED_RUSTFLAGS") {
         for flag in rustflags.to_string_lossy().split('\x1f') {
             if flag.ends_with("wasi-exec-model=reactor") {
