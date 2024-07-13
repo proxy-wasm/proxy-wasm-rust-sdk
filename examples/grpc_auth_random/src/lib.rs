ff92af940fbb59f2008e2c672df1698d577ef735
Piotr Sikora <piotr.sikora@frickle.com>
2024-07-13 00:44:15 -0400

    Add example using gRPC callouts.
    Signed-off-by: Piotr Sikora <piotr.sikora@frickle.com>

---
+++ examples/grpc_auth_random/src/lib.rs
@@ -0,0 +1,61 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use log::info;
+use proxy_wasm::traits::*;
+use proxy_wasm::types::*;
+use std::time::Duration;
+
+proxy_wasm::main! {{
+    proxy_wasm::set_log_level(LogLevel::Trace);
+    proxy_wasm::set_http_context(|_, _| -> Box<dyn HttpContext> { Box::new(GrpcAuthRandom) });
+}}
+
+struct GrpcAuthRandom;
+
+impl HttpContext for GrpcAuthRandom {
+    fn on_http_request_headers(&mut self, _: usize, _: bool) -> Action {
+        self.dispatch_grpc_call(
+            "grpcbin",
+            "grpcbin.GRPCBin",
+            "RandomError",
+            vec![],
+            None,
+            Duration::from_secs(1),
+        )
+        .unwrap();
+        Action::Pause
+    }
+
+    fn on_http_response_headers(&mut self, _: usize, _: bool) -> Action {
+        self.set_http_response_header("Powered-By", Some("proxy-wasm"));
+        Action::Continue
+    }
+}
+
+impl Context for GrpcAuthRandom {
+    fn on_grpc_call_response(&mut self, _: u32, status_code: u32, _: usize) {
+        if status_code % 2 == 0 {
+            info!("Access granted.");
+            self.resume_http_request();
+        } else {
+            info!("Access forbidden.");
+            self.send_http_response(
+                403,
+                vec![("Powered-By", "proxy-wasm")],
+                Some(b"Access forbidden.\n"),
+            );
+        }
+    }
+}
